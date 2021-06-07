3e90ac46939a8dc8eacdac3a15d9c16f5942ef49
Rafael Zago <rafael.zago@br-mediagroup.com>
2021-06-07 12:13:08 -0300

    feat: create HttpPostClientSpy

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,21 +1,12 @@
-import { HttpPostClient } from "@/data/protocols/http/http-post-client";
-import { RemoteAuthentication } from "./remote-authentication";
-
+import { HttpPostClientSpy } from '../../tests/mock-http-client'
+import { RemoteAuthentication } from './remote-authentication'
 
 describe('RemoteAuthentication', () => {
   test('Should call HttpClient with correct URL', async () => {
-    class HttpPostClientSpy implements HttpPostClient {
-      url?: string
-      async post (url: string): Promise<void> {
-        this.url = url
-        return Promise.resolve()
-      }
-    }
-
     const url = 'any_url'
     const httpPostClient = new HttpPostClientSpy()
     const sut = new RemoteAuthentication(url, httpPostClient)
     await sut.auth()
     expect(httpPostClient.url).toBe(url)
-  });
-});
+  })
+})
