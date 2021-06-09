1f846b41c22a82215de06ab3a0118f0e89e03616
Rafael Zago <rafael.zago@br-mediagroup.com>
2021-06-09 09:53:47 -0300

    feat: create form context

---
+++ src/presentation/contexts/forms/form-context.ts
@@ -0,0 +1,13 @@
+import { createContext } from 'react'
+
+export type FormContextParams = {
+  isLoading: boolean
+  errorMessage?: string
+  successMessage?: string
+}
+
+export const FormContext = createContext<FormContextParams>({
+  isLoading: false,
+  errorMessage: '',
+  successMessage: '',
+})
