6b90d169128a8cdbcf5bb02021983a2901a45218
Rafael Zago <rafael.zago@br-mediagroup.com>
2021-06-09 13:08:00 -0300

    refactor: configure webpack

---
+++ webpack.dev.js
@@ -0,0 +1,48 @@
+const { DefinePlugin } = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common')
+
+module.exports = merge(common, {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: { modules: true }
+          },
+          { loader: 'sass-loader' }
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader','css-loader']
+      },
+    ]
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './public',
+    writeToDisk: true,
+    historyApiFallback: true,
+    port: 8080
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './template.dev.html'
+    })
+  ]
+})
