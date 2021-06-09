b5d9b6420b96dbb6ab7b73372aef6f8c38e4b4dc
Rafael Zago <rafael.zago@br-mediagroup.com>
2021-06-09 09:53:16 -0300

    chore: configure testing-library package

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,20 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/main/**/*',
+    '!<rootDir>/src/**/index.ts',
+    '!**/*.d.ts'
+  ],
   coverageDirectory: 'coverage',
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+  ],
   testEnvironment: 'jsdom',
   transform: {
     '.+\\.(ts|tsx)$': 'ts-jest'
   },
+  setupFilesAfterEnv: ["./jest.setup.js"],
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1',
   }
