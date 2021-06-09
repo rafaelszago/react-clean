b51f08edc8c7e056d16828f615a118db503c4601
Rafael Zago <rafael.zago@br-mediagroup.com>
2021-06-09 14:59:19 -0300

    chore: change jest setup

--- src/main/config/jest.setup.js
+++ src/main/config/jest.setup.js
@@ -1,1 +1,7 @@
-import '@testing-library/jest-dom'
+global.matchMedia = global.matchMedia || function () {
+  return {
+    matches: false,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+  };
+};
