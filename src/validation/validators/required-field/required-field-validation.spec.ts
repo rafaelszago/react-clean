f90e0902617a04766b284457885ba563539a16c8
Rafael Zago <rafael.zago@br-mediagroup.com>
2021-06-10 14:25:25 -0300

    test: should return falsy if field is valid

--- src/validation/validators/required-field/required-field-validation.spec.ts
+++ src/validation/validators/required-field/required-field-validation.spec.ts
@@ -1,10 +1,17 @@
+import faker from 'faker'
 import { RequiredFieldError } from '@/validation/errors'
 import { RequiredFieldValidation } from './required-field-validation'
 
 describe('RequiredFieldValidation', () => {
-  test('Should return errror if field is empty', () => {
+  test('Should return error if field is empty', () => {
     const sut = new RequiredFieldValidation('email')
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFieldError('email'))
+  })
+
+  test('Should return falsy if field is valid', () => {
+    const sut = new RequiredFieldValidation('email')
+    const error = sut.validate(faker.random.words())
+    expect(error).toBeFalsy
   })
 })
