7dd45ed492fa276cc3b4e85c268f6204bff6632c
Rafael Zago <rafael.zago@br-mediagroup.com>
2021-06-10 16:37:06 -0300

    refactor: change text

--- src/validation/errors/min-length-error.ts
+++ src/validation/errors/min-length-error.ts
@@ -1,6 +1,6 @@
 export class MinLengthError extends Error {
   constructor(readonly name: string, readonly minLength: string) {
-    super(`Input ${name} need ${minLength} at least`)
+    super(`Input ${name} need ${minLength} characters at least`)
     this.name = 'MinLengthError'
   }
 }
