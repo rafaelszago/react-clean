87555f93a3e4edef912868bdd517538163f34e93
Rafael Zago <rafael.zago@br-mediagroup.com>
2021-06-10 16:58:01 -0300

    refactor: move sut to a factory method

--- src/validation/validators/max-length/max-length-validation.spec.ts
+++ src/validation/validators/max-length/max-length-validation.spec.ts
@@ -2,9 +2,12 @@ import faker from 'faker'
 import { MaxLengthError } from '@/validation/errors'
 import { MaxLengthValidation } from './max-length-validation'
 
+const makeSut = (): MaxLengthValidation =>
+  new MaxLengthValidation(faker.database.column(), 5)
+
 describe('MinLengthValidation', () => {
   test('Should return error if maxLength is invalid', () => {
-    const sut = new MaxLengthValidation(faker.database.column(), 5)
+    const sut = makeSut()
     const error = sut.validate(faker.random.alphaNumeric(10))
     expect(error).toEqual(new MaxLengthError(sut.name, sut.minLength))
   })
--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -2,9 +2,12 @@ import faker from 'faker'
 import { MinLengthError } from '@/validation/errors'
 import { MinLengthValidation } from './min-length-validation'
 
+const makeSut = (): MinLengthValidation =>
+  new MinLengthValidation(faker.database.column(), 10)
+
 describe('MinLengthValidation', () => {
   test('Should return error if minLength is invalid', () => {
-    const sut = new MinLengthValidation(faker.database.column(), 10)
+    const sut = makeSut()
     const error = sut.validate(faker.random.alphaNumeric(6))
     expect(error).toEqual(new MinLengthError(sut.name, sut.minLength))
   })
