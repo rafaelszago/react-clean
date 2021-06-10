62f03e0550275f3836e553c10a281a6e8d147161
Rafael Zago <rafael.zago@br-mediagroup.com>
2021-06-10 16:35:52 -0300

    test: create minLength validation

---
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -0,0 +1,14 @@
+import faker from 'faker'
+import { MinLengthError } from '@/validation/errors'
+import { MinLengthValidation } from './min-length-validation'
+
+describe('MinLengthValidation', () => {
+  test('Should return error if minLength is invalid', () => {
+    const sut = new MinLengthValidation(
+      faker.database.column(),
+      faker.random.alphaNumeric(5),
+    )
+    const error = sut.validate(faker.random.alphaNumeric(6))
+    expect(error).toEqual(new MinLengthError(sut.name, sut.minLength))
+  })
+})
