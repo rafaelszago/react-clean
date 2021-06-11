b27541f61ba3f1befb2b9bb485170d6662e0b0ef
Rafael Zago <rafael.zago@br-mediagroup.com>
2021-06-11 14:22:05 -0300

    refactor: add param

--- src/presentation/contexts/forms/signin-context.ts
+++ src/presentation/contexts/forms/signin-context.ts
@@ -3,6 +3,7 @@ import { createContext } from 'react'
 export type SigninContextParams = {
   email: string
   password: string
+  isLoading?: boolean
   emailError?: string
   passwordError?: string
 }
