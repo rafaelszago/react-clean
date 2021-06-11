a73aad3b8f304e47893e9d6b3d662d6d5d89a963
Rafael Zago <rafael.zago@br-mediagroup.com>
2021-06-11 08:54:48 -0300

    test: ensure ValidationComposite returns error if fails

---
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -0,0 +1,19 @@
+import faker from 'faker'
+import { ValidationComposite } from './validation-composite'
+import { FieldValidationSpy } from '../tests/mock-field-validation'
+
+describe('ValidationComposite', () => {
+  test('Should return error if any validation fails', () => {
+    const fieldName = faker.database.column()
+    const fieldValidationSpy = new FieldValidationSpy(fieldName)
+    const fieldValidationSpy2 = new FieldValidationSpy(fieldName)
+    const errorMessage = faker.hacker.phrase()
+    fieldValidationSpy2.error = new Error(errorMessage)
+    const sut = new ValidationComposite([
+      fieldValidationSpy,
+      fieldValidationSpy2,
+    ])
+    const error = sut.validate(fieldName, faker.random.word())
+    expect(error).toBe(errorMessage)
+  })
+})
