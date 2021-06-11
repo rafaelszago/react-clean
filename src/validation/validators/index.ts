89ac2cd9b6721d749874950f9357e2bb6f2f803b
Rafael Zago <rafael.zago@br-mediagroup.com>
2021-06-11 10:21:25 -0300

    refactor: export validators

--- src/validation/validators/index.ts
+++ src/validation/validators/index.ts
@@ -1,2 +1,5 @@
-export * from './required-field/required-field-validation'
+export * from './composite/validation-composite'
 export * from './email/email-validation'
+export * from './max-length/max-length-validation'
+export * from './min-length/min-length-validation'
+export * from './required-field/required-field-validation'
