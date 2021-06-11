22b9bc2e2f24a35b1c64fcdc6bf6d07ceb43332a
Rafael Zago <rafael.zago@br-mediagroup.com>
2021-06-11 14:49:14 -0300

    test: ensure call AuthenticationSpy with correct params

--- src/presentation/pages/signin/singin.spec.tsx
+++ src/presentation/pages/signin/singin.spec.tsx
@@ -7,25 +7,30 @@ import {
 } from '@testing-library/react'
 import faker from 'faker'
 import Signin from './signin'
-import { ValidationStub } from '@/presentation/tests'
+import { AuthenticationSpy, ValidationStub } from '@/presentation/tests'
 
 type SutTypes = {
   sut: RenderResult
   validationStub: ValidationStub
+  authenticationSpy: AuthenticationSpy
 }
 
 const makeSut = (): SutTypes => {
   const validationStub = new ValidationStub()
-  const sut = render(<Signin validation={validationStub} />)
+  const authenticationSpy = new AuthenticationSpy()
+  const sut = render(
+    <Signin validation={validationStub} authentication={authenticationSpy} />,
+  )
 
   return {
     sut,
     validationStub,
+    authenticationSpy,
   }
 }
 
 describe('Signin Component', () => {
-  test('Should render login component', () => {
+  test('Should render correct inputs component', () => {
     const { sut } = makeSut()
     const emailInput = sut.getByTestId('email')
     const passwordInput = sut.getByTestId('password')
@@ -39,10 +44,12 @@ describe('Signin Component', () => {
     const emailInput = sut.getByTestId('email')
     const passwordInput = sut.getByTestId('password')
     const submitButton = sut.getByTestId('submit') as HTMLButtonElement
+
     fireEvent.input(emailInput, { target: { value: faker.random.word() } })
     fireEvent.input(passwordInput, {
       target: { value: faker.internet.password() },
     })
+
     await waitFor(() => {
       expect(submitButton.disabled).toBe(true)
     })
@@ -52,14 +59,38 @@ describe('Signin Component', () => {
     const { sut } = makeSut()
     const emailInput = sut.getByTestId('email')
     const passwordInput = sut.getByTestId('password')
-    const submitButton = sut.getByTestId('submit')
+    const submitButton = sut.getByTestId('submit') as HTMLButtonElement
+
     fireEvent.input(emailInput, { target: { value: faker.internet.email() } })
     fireEvent.input(passwordInput, {
       target: { value: faker.internet.password() },
     })
     fireEvent.click(submitButton)
+
     await waitFor(() => {
       expect(submitButton.childNodes).toHaveLength(2)
+    })
+  })
+
+  test('Should call AuthenticationSpy with correct params', async () => {
+    const { sut, authenticationSpy } = makeSut()
+    const emailInput = sut.getByTestId('email')
+    const passwordInput = sut.getByTestId('password')
+    const submitButton = sut.getByTestId('submit') as HTMLButtonElement
+    const email = faker.internet.email()
+    const password = faker.internet.password()
+
+    fireEvent.input(emailInput, { target: { value: email } })
+    fireEvent.input(passwordInput, {
+      target: { value: password },
+    })
+    fireEvent.click(submitButton)
+
+    await waitFor(() => {
+      expect(authenticationSpy.params).toEqual({
+        email,
+        password,
+      })
     })
   })
 })
