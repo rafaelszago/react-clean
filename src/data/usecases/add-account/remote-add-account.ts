5f86f5875189ef58336c2aa1dc5765d7bb5584ac
Rafael Zago <rafael.zago@br-mediagroup.com>
2021-06-14 18:22:55 -0300

    feat: create RemoteAddAccount

--- src/data/usecases/add-account/remote-add-account.ts
+++ src/data/usecases/add-account/remote-add-account.ts
@@ -1,13 +1,30 @@
-import faker from 'faker'
-import { AddAccountParams } from '@/domain/usecases'
+import { AddAccount, AddAccountParams } from '@/domain/usecases'
+import { HttpPostClient, HttpStatusCode } from '@/data/protocols/http'
+import { AccountModel } from '@/domain/models'
+import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
 
-export const mockAddAccountParams = (): AddAccountParams => {
-  const password = faker.internet.password()
+export class RemoteAddAccount implements AddAccount {
+  constructor(
+    private readonly url: string,
+    private readonly httpPostClient: HttpPostClient<
+      AddAccountParams,
+      AccountModel
+    >
+  ) {}
 
-  return {
-    name: faker.name.findName(),
-    email: faker.internet.email(),
-    password,
-    passwordConfirmation: password
+  async add(params: AddAccountParams): Promise<AccountModel> {
+    const httpResponse = await this.httpPostClient.post({
+      url: this.url,
+      body: params
+    })
+
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.success:
+        return httpResponse.body
+      case HttpStatusCode.forbidden:
+        throw new InvalidCredentialsError()
+      default:
+        throw new UnexpectedError()
+    }
   }
 }
