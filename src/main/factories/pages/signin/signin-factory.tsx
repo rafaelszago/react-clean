33ef6d26a00a28b701a77d8723f2adb0cb226739
Rafael Zago <rafael.zago@br-mediagroup.com>
2021-06-14 15:38:33 -0300

    feat: implement LocalStorage factory to signin

--- src/main/factories/pages/signin/signin-factory.tsx
+++ src/main/factories/pages/signin/signin-factory.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Signin from '@/presentation/pages/signin/signin'
 import { makeRemoteAuthentication } from '../../usecases/authentication/remote-authentication-factory'
 import { makeLoginValidation } from './signin-validation-factory'
+import { makeLocalSaveAccessToken } from '../../usecases/save-access-token/local-save-access-token-factory'
 
 export const makeLogin: React.FC = () => {
   return (
     <Signin
       authentication={makeRemoteAuthentication()}
       validation={makeLoginValidation()}
+      saveAccessToken={makeLocalSaveAccessToken()}
     />
   )
 }
