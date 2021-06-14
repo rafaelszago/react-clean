ba7a78c768e3d2b117fb60f6a60dd346ca96b1e1
Rafael Zago <rafael.zago@br-mediagroup.com>
2021-06-14 17:05:22 -0300

    test: ensure HttpPostClient call with correct body

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -31,4 +31,12 @@ describe('RemoteAddAccount', () => {
 
     expect(httpPostClientSpy.url).toBe(url)
   })
+
+  test('Should call HttpPostClinet with correct body', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    const addAccountParams = mockAddAccountParams()
+    await sut.add(addAccountParams)
+
+    expect(httpPostClientSpy.body).toBe(addAccountParams)
+  })
 })
