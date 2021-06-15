28adb683f172fcef7ff33fc2fcd3a0f04d2f56de
Rafael Zago <rafael.zago@br-mediagroup.com>
2021-06-15 11:21:24 -0300

    feat: make signup factory

--- src/main/factories/pages/signup/signup-factory.tsx
+++ src/main/factories/pages/signup/signup-factory.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import SignUp from '@/presentation/pages/signup/signup'
-import { makeRemoteAuthentication } from '../../usecases/authentication/remote-authentication-factory'
 import { makeSignUpValidation } from './signup-validation-factory'
-import { makeLocalSaveAccessToken } from '../../usecases/save-access-token/local-save-access-token-factory'
+import { makeRemoteAddAccount, makeLocalSaveAccessToken } from '../../usecases'
 
 export const makeSignUp: React.FC = () => {
   return (
     <SignUp
-      authentication={makeRemoteAuthentication()}
+      addAccount={makeRemoteAddAccount()}
       validation={makeSignUpValidation()}
       saveAccessToken={makeLocalSaveAccessToken()}
     />
