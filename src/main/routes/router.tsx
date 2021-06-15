4501d744589b2651b9e7a39c850aaac052b19579
Rafael Zago <rafael.zago@br-mediagroup.com>
2021-06-15 08:23:20 -0300

    feat: create signup route

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { makeLogin } from '../factories/pages/signin/signin-factory'
+import { makeSignIn } from '../factories/pages/signin/signin-factory'
+import { makeSignUp } from '../factories/pages/signup/signup-factory'
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/signin" exact component={makeLogin} />
+        <Route path="/signin" exact component={makeSignIn} />
+        <Route path="/signup" exact component={makeSignUp} />
       </Switch>
     </BrowserRouter>
   )
