e5e71a782dc78868c7095b7dd11b231b6be30129
Rafael Zago <rafael.zago@br-mediagroup.com>
2021-06-15 17:27:07 -0300

    feat: change import

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -2,6 +2,6 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import Router from './routes/router'
 
-import 'antd/dist/antd.css'
+import '@/presentation/styles/app.less'
 
 ReactDOM.render(<Router />, document.getElementById('main'))
