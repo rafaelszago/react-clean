cf2127b0976cec9f0c022752b4985e07db51824b
Rafael Zago <rafael.zago@br-mediagroup.com>
2021-06-16 12:27:58 -0300

    test: ensure SignIn load with correct values

--- src/main/tests/cypress/integration/signin.spec.ts
+++ src/main/tests/cypress/integration/signin.spec.ts
@@ -1,5 +1,11 @@
 describe('SignIn', () => {
-  it('Should load with correct initial state', () => {
+  beforeEach(() => {
     cy.visit('signin')
+  })
+
+  it('Should load with correct initial state', () => {
+    cy.get('[data-testid="email"]').should('have.value', '')
+    cy.get('[data-testid="password"]').should('have.value', '')
+    cy.get('[data-testid="submit"]').should('have.attr', 'disabled')
   })
 })
