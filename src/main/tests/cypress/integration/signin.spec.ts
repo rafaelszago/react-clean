bd4e5ba92a4fd8bdcc8f6587696a14bd3664573b
Rafael Zago <rafaelszago@gmail.com>
2021-06-17 12:59:50 -0300

    refactor: add cypress custom methods

--- src/main/tests/cypress/integration/signin.spec.ts
+++ src/main/tests/cypress/integration/signin.spec.ts
@@ -1,3 +1,4 @@
+import { EmailError, MinLengthError } from '@/validation/errors'
 import faker from 'faker'
 import { mockInvalidCredentialsError, mockSuccess } from '../support/http-mocks'
 
@@ -7,37 +8,35 @@ describe('SignIn', () => {
   })
 
   it('Should load with correct initial state', () => {
-    cy.get('[data-testid="email"]').should('have.value', '')
-    cy.get('[data-testid="password"]').should('have.value', '')
-    cy.get('[data-testid="submit"]').should('have.attr', 'disabled')
+    cy.getByTestId('email').should('have.value', '')
+    cy.getByTestId('password').should('have.value', '')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
   })
 
   it('Should show error if form is invalid', () => {
-    cy.get('[data-testid="email"]').type(faker.random.word())
-    cy.get('[data-testid="password"]').type(faker.random.alpha({ count: 5 }))
-    cy.get('[data-testid="email-item"]')
-      .children('.ant-form-item-control')
-      .children('.ant-form-item-explain-error')
-      .children()
-      .should('contain.text', 'E-mail is invalid')
-    cy.get('[data-testid="password-item"]')
-      .children('.ant-form-item-control')
-      .children('.ant-form-item-explain-error')
-      .children()
-      .should('contain.text', 'Input password need 6 characters at least')
+    cy.getByTestId('email').type(faker.random.word())
+    cy.getByTestId('password').type(faker.random.alpha({ count: 5 }))
+    cy.getInputErrorMessage('email-item').should(
+      'contain.text',
+      new EmailError().message
+    )
+    cy.getInputErrorMessage('password-item').should(
+      'contain.text',
+      new MinLengthError('password', 6)
+    )
   })
 
   it('Should enable submit button if form is valid', () => {
-    cy.get('[data-testid="email"]').type(faker.internet.email())
-    cy.get('[data-testid="password"]').type(faker.random.alpha({ count: 6 }))
-    cy.get('[data-testid="submit"]').should('not.have.attr', 'disabled')
+    cy.getByTestId('email').type(faker.internet.email())
+    cy.getByTestId('password').type(faker.random.alpha({ count: 6 }))
+    cy.getByTestId('submit').should('not.have.attr', 'disabled')
   })
 
   it('Should has no accessToken if credentials are invalid', () => {
     mockInvalidCredentialsError({ url: 'api/signin' })
-    cy.get('[data-testid="email"]').type(faker.internet.email())
-    cy.get('[data-testid="password"]').type(faker.random.alpha({ count: 6 }))
-    cy.get('[data-testid="submit"]').click()
+    cy.getByTestId('email').type(faker.internet.email())
+    cy.getByTestId('password').type(faker.random.alpha({ count: 6 }))
+    cy.getByTestId('submit').click()
     cy.wait('@apiRequest')
       .window()
       .then(window => assert.isNull(window.localStorage.getItem('accessToken')))
@@ -45,9 +44,9 @@ describe('SignIn', () => {
 
   it('Should save accessToken if request returns status 200', () => {
     mockSuccess({ url: 'api/signin' })
-    cy.get('[data-testid="email"]').type(faker.internet.email())
-    cy.get('[data-testid="password"]').type(faker.random.alpha({ count: 6 }))
-    cy.get('[data-testid="submit"]').click()
+    cy.getByTestId('email').type(faker.internet.email())
+    cy.getByTestId('password').type(faker.random.alpha({ count: 6 }))
+    cy.getByTestId('submit').click()
     cy.wait('@apiRequest')
       .window()
       .then(window =>
